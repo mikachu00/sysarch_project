8f005255f523d463faaee8ea03c985df6a98a778
mikachu00 <maykylacabanog@gmail.com>
2025-03-24 12:43:17 +0800

    Connecting Database

--- Connection/Connection.cs
+++ Connection/Connection.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MidtermProject.Connection
 {
     internal class Connection
     {
+        public static OleDbConnection conn;
+        public static string dbconnect = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\\MIDTERM.accdb";
+        //rovider=Microsoft.ACE.OLEDB.12.0;Data Source = C:\Users\user\source\repos\MidtermProject\bin\Debug\MIDTERM.accdb
+
+        public static void DB()
+        {
+            try
+            {
+                conn = new OleDbConnection(dbconnect);
+                conn.Open();
+
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
